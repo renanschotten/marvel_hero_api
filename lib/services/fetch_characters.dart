011d19656ec23a06546ec5d40e7eb214a9a165de
Renan Schotten <renan.schotten@gmail.com>
2023-03-17 09:58:58 -0300

    feature: created service

--- lib/services/fetch_characters.dart
+++ lib/services/fetch_characters.dart
@@ -1,9 +1,14 @@
 import 'package:dartz/dartz.dart';
+
 import 'package:marvel_hero_api/models/character.dart';
 import 'package:marvel_hero_api/repositories/character_repository.dart';
 
 class CharactersService {
-  Future<Either<String, List<Character>>> fetchCharacters() {
-    return CharacterRepositoryImpl().fetchCharacters();
+  final CharacterRepository repository;
+
+  CharactersService({required this.repository});
+
+  Future<Either<String, List<Character>>> fetchCharacters() async {
+    return await repository.fetchCharacters();
   }
 }
